162be2a7daf291783e22b17d75008a00354dc44d
quang <vudinhquangk53@gmail.com>
2019-09-04 10:35:40 +0900

    Init Intersection Observer API

---
+++ intersectionObserverApi/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Lazy Load</title>
+    </head>
+    <body>
+        <div style="text-align: center">
+            <img src="images/1.jpg"><br>
+            <img src="images/2.jpg"><br>
+            <img src="images/3.jpg"><br>
+            <img src="images/4.jpg"><br>
+            <img src="images/5.jpg"><br>
+            <img src="images/6.jpg"><br>
+            <img src="images/7.png"><br>
+            <img src="images/8.jpg"><br>
+            <img src="images/9.jpg"><br>
+            <img src="images/10.jpg"><br>
+            <img src="images/11.jpg"><br>
+            <img src="images/12.jpg"><br>
+            <img src="images/13.png"><br>
+            <img src="images/14.jpg">
+        </div>
+    </body>
+</html>
